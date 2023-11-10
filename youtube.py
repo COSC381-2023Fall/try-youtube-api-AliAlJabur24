9211403f3eff69025cac2605ff5def6ca3067379
AliAlJabur24 <66652142+AliAlJabur24@users.noreply.github.com>
2023-11-09 22:32:07 -0500

    Allow user to pick page number

--- youtube.py
+++ youtube.py
@@ -7,7 +7,7 @@ DEVELOPER_KEY = config.API_KEY
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def youtube_search(query_term, max_results):
+def youtube_search(query_term, max_results, page_number):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                     developerKey=DEVELOPER_KEY)
 
@@ -19,47 +19,52 @@ def youtube_search(query_term, max_results):
         maxResults=max_results,
     ).execute()
 
-    # Function to extract video details from search response
-    def extract_video_details(search_response):
-        videoList = []
-        for search_result in search_response.get('items', []):
-            video_info = {
-                'videoId': search_result['id']['videoId'],
-                'title': search_result['snippet']['title'],
-                'description': search_result['snippet']['description'],
-                'thumbnail': search_result['snippet']['thumbnails']['default']['url'],
-                'publishedAt': search_result['snippet']['publishedAt']
-            }
-            videoList.append(video_info)
-        return videoList
-
-    # # extract video details for page 1
-    first_page_videos = extract_video_details(search_response)
-
-    # Attempt to get the second page of videos
-    next_page_token = search_response.get('nextPageToken', '')
-    second_page_videos = []
-
-    if next_page_token:
-        # get the new results with the next page
+    page_token = None  # Initialize page_token None for the first iteration
+    current_page = 0
+    while current_page < page_number:
         search_response = youtube.search().list(
             q=query_term,
             part='id,snippet',
             maxResults=max_results,
             type='video',
-            pageToken=next_page_token
+            pageToken=page_token
         ).execute()
 
-        # extract video details for page 2
-        second_page_videos = extract_video_details(search_response)
+        page_token = search_response.get('nextPageToken')
+        current_page += 1
+        if not page_token:  # Break loop if there is not a page token
+            break
 
-    # Return both lists of page video details
-    return first_page_videos, second_page_videos
+    if current_page != page_number:
+        raise ValueError("The specified page number is out of range.")
+
+    # Extract video information from the search response
+    videos = []
+    for search_result in search_response.get('items', []):
+        video_info = {
+            'videoId': search_result['id']['videoId'],
+            'title': search_result['snippet']['title'],
+            'description': search_result['snippet']['description'],
+            'thumbnail': search_result['snippet']['thumbnails']['default']['url'],
+            'publishedAt': search_result['snippet']['publishedAt']
+        }
+        videos.append(video_info)
+
+    return videos
 
 if __name__ == "__main__":
     query_term = sys.argv[1]
-    max_results = sys.argv[2]
+    max_results = int(sys.argv[2])  
+    page_number = int(sys.argv[3])
+
+    if page_number < 1:
+        raise ValueError("Page number must be a positive integer.")
+
     try:
-        print(youtube_search(query_term, max_results))
+        videos = youtube_search(query_term, max_results, page_number)
+        print(f"Page {page_number} results:")
+        print(videos)
     except HttpError as e:
         print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+    except ValueError as e:
+        print(e)
